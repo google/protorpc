f9962d8b272832839e376e2074de7fcbde9018bc
Craig Citro <craigcitro@google.com>
2013-10-16 15:02:56 -0700

    Add the ability to initialize MessageFields with a dict.

--- python/protorpc/messages_test.py
+++ python/protorpc/messages_test.py
@@ -1215,6 +1215,43 @@ class MessageTest(test_util.TestCase):
     self.assertTrue(simple_message.is_initialized())
     self.assertTrue(nested_message.is_initialized())
 
+  def testInitializeNestedFieldFromDict(self):
+    """Tests initializing nested fields from dict."""
+    class SimpleMessage(messages.Message):
+      required = messages.IntegerField(1, required=True)
+
+    class NestedMessage(messages.Message):
+      simple = messages.MessageField(SimpleMessage, 1)
+
+    class RepeatedMessage(messages.Message):
+      simple = messages.MessageField(SimpleMessage, 1, repeated=True)
+
+    nested_message1 = NestedMessage(simple={'required': 10})
+    self.assertTrue(nested_message1.is_initialized())
+    self.assertTrue(nested_message1.simple.is_initialized())
+
+    nested_message2 = NestedMessage()
+    nested_message2.simple = {'required': 10}
+    self.assertTrue(nested_message2.is_initialized())
+    self.assertTrue(nested_message2.simple.is_initialized())
+
+    repeated_values = [{}, {'required': 10}, SimpleMessage(required=20)]
+
+    repeated_message1 = RepeatedMessage(simple=repeated_values)
+    self.assertEquals(3, len(repeated_message1.simple))
+    self.assertFalse(repeated_message1.is_initialized())
+
+    repeated_message1.simple[0].required = 0
+    self.assertTrue(repeated_message1.is_initialized())
+
+    repeated_message2 = RepeatedMessage()
+    repeated_message2.simple = repeated_values
+    self.assertEquals(3, len(repeated_message2.simple))
+    self.assertFalse(repeated_message2.is_initialized())
+
+    repeated_message2.simple[0].required = 0
+    self.assertTrue(repeated_message2.is_initialized())
+
   def testNestedMethodsNotAllowed(self):
     """Test that method definitions on Message classes are not allowed."""
     def action():
