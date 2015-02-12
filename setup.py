86b01462502c3b07359b3b1770855c9de1e27bb0
Tres Seaver <tseaver@palladion.com>
2015-02-12 14:52:43 -0500

    This is not your father's 0.9.1.

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ py_version = platform.python_version()
 if py_version < '2.6':
   REQUIRED_PACKAGES.append('simplejson')
 
-_PROTORPC_VERSION = '0.9.1'
+_PROTORPC_VERSION = '0.9.2a1'
 packages = [
     'protorpc',
 ]
